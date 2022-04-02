630c01ef422a6e96b5e8b00ba6c67c43b4f65eee
Arian N. Yamchelo <ariannajafi@outlook.com>
2022-04-03 08:51:37 +1000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module app.smartboard {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens app.smartboard to javafx.fxml;
+    exports app.smartboard;
+}
